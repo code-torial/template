7b2e7f07894eb278f58559d2fc06644182263f9d
Anthony Gore <anthony@vuejsdevelopers.com>
2022-05-19 12:13:38 +1000

    Updating index

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import VitePluginHtmlEnv from 'vite-plugin-html-env'
+import { ViteEjsPlugin } from "vite-plugin-ejs"
 
 export default {
   plugins: [
-    VitePluginHtmlEnv(),
+    ViteEjsPlugin(({ server, env }) => {
+      return {
+        isDev: env.DEV,
+        local: env.VITE_APP_LOCAL || false,
+        appUrl: env.VITE_APP_URL || 'https://codetorial-app.netlify.app',
+        title: env.VITE_APP_TITLE || 'Codetorial',
+        faviconUrl: env.VITE_APP_FAVICON_URL || '/favicon.ico',
+        baseUrl: env.VITE_APP_BASE_URL || `http://localhost:${server.port}`,
+        homeUrl: env.VITE_APP_HOME_URL || 'https://codetorial.netlify.app',
+        logoUrl: env.VITE_APP_LOGO_URL || '/logo.png'
+      }
+    }),
   ]
 }
