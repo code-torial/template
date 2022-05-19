fba5215ef7d6c96738b4ebfcd1f5e507d6c16e31
Anthony Gore <anthony@vuejsdevelopers.com>
2022-05-19 14:42:49 +1000

    Updating for multi course

--- vite.config.js
+++ vite.config.js
@@ -9,9 +9,7 @@ export default {
         appUrl: env.VITE_APP_URL || 'https://codetorial-app.netlify.app',
         title: env.VITE_APP_TITLE || 'Codetorial',
         faviconUrl: env.VITE_APP_FAVICON_URL || '/favicon.png',
-        baseUrl: env.VITE_APP_BASE_URL || `http://localhost:${server.port}`,
-        homeUrl: env.VITE_APP_HOME_URL || 'https://codetorial.netlify.app',
-        logoUrl: env.VITE_APP_LOGO_URL || '/logo.png'
+        baseUrl: env.VITE_APP_BASE_URL || `http://localhost:${server.port}`
       }
     }),
   ]
